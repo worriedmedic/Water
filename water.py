5b025ed7ba3d7f5e2fb0af0a546385fa7551932e
worriedmedic <comical115@gmail.com>
2017-05-31 19:18:34 -0400

    Update water.py

--- water.py
+++ water.py
@@ -23,19 +23,36 @@ ser.readline()
 ser.readline()
 ser.readline()
 
-while(1):
+while True:
 	now = time.strftime("%H:%M:%S")
 	today = datetime.date.today()
-	buffer = ser.readline()
-	buffer = buffer.strip("\n")
-	x = str(today) + ',' + str(now) + ',' + str(buffer)
-	flowrate = buffer.split(',')[1]
-	liquidflowing = buffer.split(',')[3]
-	totaloutput = buffer.split(',')[5].strip('\r')
-	print(x)
-	#if verbose:
-	#	print("Current Flow Rate (mL/min): ", flowrate)
-	#	print("Current Liquid Flowing (mL): ", liquidflowing)
-	#	print("Total Liquid Output (mL): ", totaloutput)
-
-
+	try:
+		buffer = ser.readline()
+		buffer = buffer.strip("\n")
+		flowrate = buffer.split(',')[1]
+		liquidflowing = buffer.split(',')[3]
+		totaloutput = buffer.split(',')[5].strip('\r')
+		if verbose:
+			print("Current Flow Rate (mL/min): ", flowrate)
+			print("Current Liquid Flowing (mL): ", liquidflowing)
+			print("Total Liquid Output (mL): ", totaloutput)
+		x = str(now) + ',' + flowrate + ',' + liquidflowing + ',' + totaloutput + '\n'
+	except Exception:
+		print("DATA ERROR", today, now, buffer)
+		traceback.print_exc(file=sys.stdout)
+		print('-' * 60)
+	try:
+		if not os.path.exists('data_log'):
+			os.makedirs('data_log')
+		fname = str(today) + '.log'  # log file to save data in
+		fdirectory = './data_log/' + time.strftime("%Y-%m")
+		fmode = 'a'  # log file mode = append
+		if not os.path.exists(fdirectory):
+			os.makedirs(fdirectory)
+		outf = open(os.path.join(fdirectory, fname), fmode)
+		outf.write(x)  # write line of text to file
+		outf.flush()  # make sure it actually gets written out
+	except Exception:
+		print("DATA LOG ERROR", today, now, buffer)
+		traceback.print_exc(file=sys.stdout)
+		print('-' * 60)
