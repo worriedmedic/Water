b6b3a07fb7d82a37d88e65395786800f63a86f12
worriedmedic <comical115@gmail.com>
2017-05-31 18:50:30 -0400

    Update water.py

--- water.py
+++ water.py
@@ -19,19 +19,27 @@ for arg in sys.argv:
 		sys.exit()
 
 while(1):
-	try:
-		now = time.strftime("%H:%M:%S")
-		today = datetime.date.today()
-		ser = serial.Serial(addr,9600)
-		buffer = ser.readline()
-		buffer = buffer.strip("\n")
-		x = str(today) + ',' + str(now) + ',' + str(buffer) + '\n'
-		if verbose:
-			print(x)
-		flowrate 	= buffer.split(',')[1]
-		liquidflowing 	= buffer.split(',')[3]
-		totaloutput 	= buffer.split(',')[5].strip('\r')
-		if verbose:
-			print("Current Flow Rate (mL/min): ", flowrate)
-			print("Current Liquid Flowing (mL): ", liquidflowing)
-			print("Total Liquid Output (mL): ", totaloutput)
+	now = time.strftime("%H:%M:%S")
+	today = datetime.date.today()
+	ser = serial.Serial(addr,9600)
+	buffer = ser.readline()
+	buffer = buffer.strip("\n")
+	x = str(today) + ',' + str(now) + ',' + str(buffer) + '\n'
+	flowrate 	= buffer.split(',')[1]
+	liquidflowing 	= buffer.split(',')[3]
+	totaloutput 	= buffer.split(',')[5].strip('\r')
+	if verbose:
+		print("Current Flow Rate (mL/min): ", flowrate)
+		print("Current Liquid Flowing (mL): ", liquidflowing)
+		print("Total Liquid Output (mL): ", totaloutput)
+	if not os.path.exists('data_log'):
+		os.makedirs('data_log')
+	fname = str(today) + '.log'
+	fdirectory = '/home/pi/Power_Monitoring/data_log/' + time.strftime("%Y-%m")
+	fmode = 'a'
+	if not os.path.exists(fdirectory):
+		os.makedirs(fdirectory)
+	outf = open(os.path.join(fdirectory, fname), fmode)
+	outf.write(x)
+	outf.flush()
+
