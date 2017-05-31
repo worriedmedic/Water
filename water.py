7f3675a47d07b38dca7109f1dcbcf2a5a7a5800e
worriedmedic <comical115@gmail.com>
2017-05-31 18:50:59 -0400

    Update water.py

--- water.py
+++ water.py
@@ -27,7 +27,7 @@ while(1):
 	x = str(today) + ',' + str(now) + ',' + str(buffer) + '\n'
 	flowrate 	= buffer.split(',')[1]
 	liquidflowing 	= buffer.split(',')[3]
-	totaloutput 	= buffer.split(',')[5].strip('\r')
+	totaloutput 	= buffer.split(',')[5]
 	if verbose:
 		print("Current Flow Rate (mL/min): ", flowrate)
 		print("Current Liquid Flowing (mL): ", liquidflowing)
